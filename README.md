cd777219d1a6439f714761e690ca14cc79df9765
ilkkamaksy <ijmakine@gmail.com>
2022-03-02 15:28:56 +0200

    containerize the services, add keycloak

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Strong auth and API Gateway
+
+### Run the services
+
+In the project root folder, run `mvn package` or `mvn clean package` if necessary. Then, go to folder `docker/` and run `docker-compose up` or `docker-compose up --build` if necessary.
--- info-service/src/main/java/fi/hiq/info/InfoServiceApplication.java
+++ info-service/src/main/java/fi/hiq/info/InfoServiceApplication.java
@@ -1,8 +1,16 @@
 package fi.hiq.info;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import fi.hiq.info.utils.UserContextInterceptor;
 
 @SpringBootApplication
 @RefreshScope
@@ -10,6 +18,22 @@ public class InfoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InfoServiceApplication.class, args);
+	}
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate(){
+		RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors == null){
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        }
+        else{
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
 	}
 
 }
