3a1ee62e0b4f014aa56111e6a8a73d58b8e18f28
ilkkamaksy <ijmakine@gmail.com>
2022-03-16 08:12:52 +0200

    cleanup

--- identity-service/src/main/java/fi/hiq/identity/oidc/controller/AuthController.java
+++ identity-service/src/main/java/fi/hiq/identity/oidc/controller/AuthController.java
@@ -1,9 +1,5 @@
 package fi.hiq.identity.oidc.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -11,89 +7,29 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import fi.hiq.identity.oidc.domain.Identity;
-import fi.hiq.identity.oidc.domain.IdentityProviderListBuilder;
 import fi.hiq.identity.oidc.domain.OidcRequestParameters;
 import fi.hiq.identity.oidc.domain.OidcResponseParameters;
-import fi.hiq.identity.oidc.domain.idp.IdentityProvider;
-import fi.hiq.identity.oidc.domain.idp.IdentityProviderList;
 import fi.hiq.identity.oidc.facade.OidcDemoFacade;
-import net.minidev.json.JSONObject;
 
-@Controller
+@RestController
 public class AuthController {
-    private final Logger logger = LoggerFactory.getLogger(AuthController.class);
+    
+	private final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     private OidcDemoFacade facade;
 
-
-    @RequestMapping("/")
-    public String welcome(HttpServletRequest request, Map<String, Object> model) throws UnsupportedEncodingException {
-
-    	request.getSession().setAttribute("backurlprefix", "");
-
-        return "welcome";
-    }
-
-    public void prepareEmbeddedMode(String language, Map<String, Object> model, HttpServletRequest request) {
-
-        IdentityProviderList idpList = new IdentityProviderListBuilder(null).build(language);
-
-    	List<IdentityProvider> idps = idpList.getIdentityProviders();
-
-    	if (idpList.getDisturbanceInfo()!=null) {
-        	String dInfo = idpList.getDisturbanceInfo().getAsString("text");
-        	String dInfo2 = dInfo.replace("\n", "<br> <br>"); 					// Make sure that if multiple disturbances they are their own lines
-        	idpList.getDisturbanceInfo().put("text", dInfo2);
-    		request.getSession().setAttribute("disturbanceinfo", "yes");
-    	}
-    	else {
-    		request.getSession().setAttribute("disturbanceinfo", "no");
-    	}
-
-        model.put("identityProviders", idps);
-        JSONObject disturbanceInfo = idpList.getDisturbanceInfo();
-        model.put("disturbanceInfo", disturbanceInfo);
-        String isbProviderInfo = idpList.getIsbProviderInfo();
-        model.put("isbProviderInfo", isbProviderInfo);
-        String isbConcent = idpList.getIsbConsent();
-        model.put("isbConsent", isbConcent);
-        String privacyNoticeLink = idpList.getPrivacyNoticeLink();
-        model.put("privacyNoticeLink", privacyNoticeLink);
-        String privacyNoticeText = idpList.getPrivacyNoticeText();
-        model.put("privacyNoticeText", privacyNoticeText);
-    }
-
-    @RequestMapping("/embedded")
-    public String embedded(HttpServletRequest request, Map<String, Object> model) throws UnsupportedEncodingException {
-
-    	String language = (String) request.getSession().getAttribute("language");
-
-    	if (language==null) {
-    		language = request.getParameter("language");
-
-    	}
-    	if (language==null) {
-    		language="fi"; // Default language is fi
-    	}
-
-    	prepareEmbeddedMode(language, model, request);
-
-        // But default GUI language, and GUI type to session (for render)
-        request.getSession().setAttribute("language", language);
-        request.getSession().setAttribute("backurlprefix", "embedded");
-
-        return "embedded";
-    }
-
-    @RequestMapping("/initFlow")
+    @RequestMapping(value="/authorize", method = RequestMethod.GET)
     public String initFlow(HttpServletRequest request, Map<String, Object> model) {
 
+    	request.getSession().setAttribute("backurlprefix", "");
+    	
         String language = "fi";
         String idp = request.getParameter("idp");
         String requestId = UUID.randomUUID().toString();
@@ -106,33 +42,34 @@ public class AuthController {
         model.put("endpointUrl", params.getEndpointUrl());
         model.put("request", params.getRequest());
         request.getSession().setAttribute("initParams", params);
-        return "post";
+        return params.getEndpointUrl() + "?request=" + params.getRequest();
     }
 
-    @RequestMapping("/finishFlow")
-    public String finishFlow(HttpServletRequest request, Map<String, Object> model) {
+    @RequestMapping(value="/token", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> finishFlow(HttpServletRequest request, Map<String, Object> model) {
         OidcRequestParameters originalParams = (OidcRequestParameters) request.getSession().getAttribute("initParams");
         OidcResponseParameters response = new OidcResponseParameters();
         response.setError(request.getParameter("error"));
         response.setState(request.getParameter("state"));
         response.setCode(request.getParameter("code"));
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.YYYY HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        String timenow = dtf.format(now);
-
         if (response.getError() == null || response.getError().length() == 0) {
             Identity identity = getFacade().extractIdentity(response, originalParams);
-            model.put("timenow", timenow);
             model.put("identity", identity);
             model.put("backurlprefix", request.getSession().getAttribute("backUrlPost"));
-            return "identity";
         } else if (response.getError().equals("cancel")) {
-        	return "welcome";
+        	model.put("error", response.getError());
         } else {
             model.put("error", response.getError());
-            return "error";
         }
+        
+        return ResponseEntity.ok(model);
+    }
+
+    @RequestMapping(value = "/jwks", method = RequestMethod.GET)
+    @ResponseBody
+    public String jwks() {
+        return getFacade().getJwks();
     }
 
     private OidcDemoFacade getFacade() {
